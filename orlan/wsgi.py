163ccf37d5b689ff48ba8af3c8e43463c0f4bcbe
Ilya001 <ilya.sonin2018@yandex.ru>
2019-06-14 00:01:42 +0300

    Правка в wsgi

--- orlan/wsgi.py
+++ orlan/wsgi.py
@@ -1,8 +1,8 @@
 import os
 import sys
 
-sys.path.append('/home/c/ck09478/django_4/public_html/')
-sys.path.append('/home/c/ck09478/django_4/django/lib/python3.4/site-packages/')
+sys.path.append('/home/c/ck09478/orlan_django_wagtail/public_html/')
+sys.path.append('/home/c/ck09478/orlan_django_wagtail/public_html/myenv/lib/python3.4/site-packages/')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'orlan.settings'
 
 import django
